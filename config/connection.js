61affbb1a4cf02afa6cdd790ddde4ed4f120c2a2
Michael O'Connor <michaeloconnor@Mjoc-Toronto-iMac.local>
2021-03-27 15:27:43 -0400

    jaqwsdb update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL ? process.env.JAWSDB_URL :
+  {
   host: 'localhost',
   port: 3306,
   user: 'root',
